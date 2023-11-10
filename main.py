9adefa008531d6991d9d1662742cb6b5021c0231
Raj Rawariya <98628762+Raj100@users.noreply.github.com>
2023-11-10 17:28:23 +0530

    commit #5

--- main.py
+++ main.py
@@ -1,26 +1,65 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings
 import speech_recognition as sr
 import openai
 import wikipedia
 import os
 import webbrowser
 import datetime
+import requests
+import pandas as pd
+import json
+import pyjokes
+import app_paths
+apps=app_paths.apps
 
+APIKey = "9e569d36d6ec41eb84543520231011"
+city="Goa"
+current_weather_api_url = "http://api.weatherapi.com/v1/current.json?key="+APIKey+"&q="+city
+current_response = requests.get(current_weather_api_url)
+print(current_response.text)
+if( current_response.status_code == 200):
+    print("Api connected successfully")
+elif (current_response.status_code ==201):
+    print("The request was successful, but the response has no content.")
+else:
+    print("Error in Api connected failed")
+
+current_data = current_response.json()
+current_df = pd.json_normalize(current_data)
+current_text = current_df["current.condition.text"].squeeze()
+current_temp= current_df["current.temp_c"].squeeze()
+current_humidity=current_df["current.humidity"]
+current_windspeed=current_df["current.wind_kph"]
+
+
+forecast_weather_api_url ="http://api.weatherapi.com/v1/forecast.json?key="+APIKey+"&q="+city+"&aqi=true&alerts=true&days=2"
+forecast_response = requests.get(forecast_weather_api_url)
+print(forecast_response.text)
+if( forecast_response.status_code == 200):
+    print("Api connected successfully")
+elif (forecast_response.status_code ==201):
+    print("The request was successful, but the response has no content.")
+else:
+    print("Error in Api connected failed")
+
+forecast_data = forecast_response.json()
+# forecast_df = pd.json_normalize(forecast_data)
+forecast_rain = forecast_data["forecast"]["forecastday"][0]["day"]["daily_will_it_rain"]
+forecast_rain_chances=forecast_data["forecast"]["forecastday"][0]["day"]["daily_chance_of_rain"]
+forecast_snow = forecast_data["forecast"]["forecastday"][0]["day"]["daily_will_it_snow"]
+forecast_snow_chances=forecast_data["forecast"]["forecastday"][0]["day"]["daily_chance_of_snow"]
+
+forecast_rain_1 = forecast_data["forecast"]["forecastday"][1]["day"]["daily_will_it_rain"]
+forecast_rain_chances_1=forecast_data["forecast"]["forecastday"][1]["day"]["daily_chance_of_rain"]
+forecast_snow_1 = forecast_data["forecast"]["forecastday"][1]["day"]["daily_will_it_snow"]
+forecast_snow_chances_1=forecast_data["forecast"]["forecastday"][1]["day"]["daily_chance_of_snow"]
+
+
+# these keys are with 0 credits use your own, mostly now no free credits are given on new signup with openai
 api_key = "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"
 openai.api_key = api_key
 
-
 def say(text):
     os.system(f"say {text}")
-
-
-# def print_hi(name):
-#     # Use a breakpoint in the code line below to debug your script.
-#     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
 
 def takeCommand():
     r = sr.Recognizer()
@@ -35,12 +74,11 @@ def takeCommand():
         except Exception as e:
             return "Some error occured"
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print("raj")
     say("Hello I am Krim")
     while True:
+        flag=0
         print("Listening...")
         text = takeCommand().lower()
         sites = [["youtube", "https://www.youtube.com/"], ["firefox", "https://www.firefox.com/"],
@@ -49,43 +87,75 @@ if __name__ == '__main__':
             if f"open {site[0]}" in text:
                 say(f"Opening {site[0]}")
                 webbrowser.open(site[1])
-        apps = [["music", "/System/Applications/Music.app"],
-                ["powerpoint", "/Applications/\"Microsoft PowerPoint.app\""],
-                ["vs code", "/Applications/\"Visual Studio Code.app\""]]
+                flag=1
         for app in apps:
             if f"open {app[0]}" in text:
                 say(f"Opening {app[0]}")
                 os.system(f"open {app[1]}")
+                flag=1
         if "the time" in text:
-            strfTime = datetime.datetime.now().strftime("%H:%M:%S")
+            strfTime = datetime.datetime.now().strftime("%I:%M %p")
             say(f"Sir the time is {strfTime}")
-        if "using ai" in text:
-            prompt = text
+        elif "date"in text:
+            say(datetime.datetime.now().strftime("%d %B %Y"))
+        elif " day"in text:
+            say(datetime.datetime.now().strftime("%A"))
+        elif "using ai" in text:
+            prompt = text.replace("using ai","")
             response = openai.Completion.create(
                 engine="text-davinci-002",  # Choose the engine that fits your needs
+                messages=[{"role": "ai assistant"}],
                 prompt=prompt,
                 max_tokens=50  # Adjust the response length as needed
             )
             translated_text = response.choices[0].text
             say(translated_text)
-        if ("search" or "what" or "who") in text:
+        elif ("search" or "what" or "who") in text:
                 text = text.replace("search", "")
                 text = text.replace("what", "")
                 text = text.replace("who", "")
                 text = text.replace("is", "")
                 text = text.replace("are", "")
-                ans = wikipedia.summary(text, sentences=1)
+                ans = wikipedia.summary(text, sentences=2)
                 print(ans)
                 say("according to wikipedia")
                 say(ans)
-        if ("you" or "your") in text:
-            say("I don't have my personal opinions.")
-        if "sent email" in text:
-            try:
-                say("what should I say")
-
-        if "close" in text:
-            say("bye")
-            break
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        elif "joke" in text:
+            ans = pyjokes.get_joke()
+            print(ans)
+            say(ans)
+        elif "rain tomorrow" in text:
+            if (forecast_rain_1 == 1):
+                say(f"It will rain tomorrow with {forecast_rain_chances_1} percent chances")
+            else:
+                say("It will not rain tomorrow")
+        elif "snow tomorrow" in text:
+            if (forecast_snow_1 == 1):
+                say(f"it will snow tomorrow with {forecast_snow_chances_1} percent chances")
+            else:
+                say("It will not snow tomorrow")
+        elif "will it rain" in text:
+            if(forecast_rain==1):
+                say(f"it will rain today with {forecast_rain_chances} percent chances")
+            else:
+                say("It will not rain today")
+        elif "will it snow" in text:
+            if(forecast_snow==1):
+                say(f"it will snow today with {forecast_snow_chances} percent chances")
+            else:
+                say("It will not snow today")
+        elif "how is the weather" in text or ("weather today" in text):
+            say(f"current weather is {current_text}")
+            say(f"temperature is {current_temp} centigrade and")
+            say(f" humidity is {current_humidity} percent")
+        elif "wind speed" in text:
+            say(f"current wind speed is {current_windspeed} kilometers per hour")
+        elif "the temperature" in text:
+            say(f"current temperature is {current_temp}")
+        elif "the humidity" in text:
+            say(f"current humidity is {current_humidity}")
+        elif ("you" or "your") in text:
+            say("I am refrained from answering personal questions.")
+        else:
+            if flag==0:
+                say("I didnt get that")
---
+++ train.py
@@ -0,0 +1,199 @@
+# !wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
+
+# read it in to inspect it
+with open('input.txt', 'r', encoding='utf-8') as f:
+    text = f.read()
+
+# here are all the unique characters that occur in this text
+chars = sorted(list(set(text)))
+vocab_size = len(chars)
+print(''.join(chars))
+print(vocab_size)
+
+# create a mapping from characters to integers
+stoi = { ch:i for i,ch in enumerate(chars) }
+itos = { i:ch for i,ch in enumerate(chars) }
+encode = lambda s: [stoi[c] for c in s] # encoder: take a string, output a list of integers
+decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
+
+print(encode("hii there"))
+print(decode(encode("hii there")))
+
+# let's now encode the entire text dataset and store it into a torch.Tensor
+import torch # we use PyTorch: https://pytorch.org
+data = torch.tensor(encode(text), dtype=torch.long)
+print(data.shape, data.dtype)
+print(data[:1000]) # the 1000 characters we looked at earier will to the GPT look like this
+
+# Let's now split up the data into train and validation sets
+n = int(0.9*len(data)) # first 90% will be train, rest val
+train_data = data[:n]
+val_data = data[n:]
+
+block_size = 8
+train_data[:block_size+1]
+
+x = train_data[:block_size]
+y = train_data[1:block_size+1]
+for t in range(block_size):
+    context = x[:t+1]
+    target = y[t]
+    print(f"when input is {context} the target: {target}")
+
+torch.manual_seed(1337)
+batch_size = 4 # how many independent sequences will we process in parallel?
+block_size = 8 # what is the maximum context length for predictions?
+
+def get_batch(split):
+    # generate a small batch of data of inputs x and targets y
+    data = train_data if split == 'train' else val_data
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([data[i:i+block_size] for i in ix])
+    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    return x, y
+
+xb, yb = get_batch('train')
+print('inputs:')
+print(xb.shape)
+print(xb)
+print('targets:')
+print(yb.shape)
+print(yb)
+
+print('----')
+
+for b in range(batch_size): # batch dimension
+    for t in range(block_size): # time dimension
+        context = xb[b, :t+1]
+        target = yb[b,t]
+        print(f"when input is {context.tolist()} the target: {target}")
+
+print(xb) # our input to the transformer
+
+import torch.nn as nn
+from torch.nn import functional as F
+
+torch.manual_seed(1337)
+
+
+class BigramLanguageModel(nn.Module):
+
+    def __init__(self, vocab_size):
+        super().__init__()
+        # each token directly reads off the logits for the next token from a lookup table
+        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+
+    def forward(self, idx, targets=None):
+
+        # idx and targets are both (B,T) tensor of integers
+        logits = self.token_embedding_table(idx)  # (B,T,C)
+
+        if targets is None:
+            loss = None
+        else:
+            B, T, C = logits.shape
+            logits = logits.view(B * T, C)
+            targets = targets.view(B * T)
+            loss = F.cross_entropy(logits, targets)
+
+        return logits, loss
+
+    def generate(self, idx, max_new_tokens):
+        # idx is (B, T) array of indices in the current context
+        for _ in range(max_new_tokens):
+            # get the predictions
+            logits, loss = self(idx)
+            # focus only on the last time step
+            logits = logits[:, -1, :]  # becomes (B, C)
+            # apply softmax to get probabilities
+            probs = F.softmax(logits, dim=-1)  # (B, C)
+            # sample from the distribution
+            idx_next = torch.multinomial(probs, num_samples=1)  # (B, 1)
+            # append sampled index to the running sequence
+            idx = torch.cat((idx, idx_next), dim=1)  # (B, T+1)
+        return idx
+
+
+m = BigramLanguageModel(vocab_size)
+logits, loss = m(xb, yb)
+print(logits.shape)
+print(loss)
+
+print(decode(m.generate(idx=torch.zeros((1, 1), dtype=torch.long), max_new_tokens=100)[0].tolist()))
+
+optimizer = torch.optim.AdamW(m.parameters(), lr=1e-3)
+
+batch_size = 32
+for steps in range(100):  # increase number of steps for good results...
+
+    # sample a batch of data
+    xb, yb = get_batch('train')
+
+    # evaluate the loss
+    logits, loss = m(xb, yb)
+    optimizer.zero_grad(set_to_none=True)
+    loss.backward()
+    optimizer.step()
+
+print(loss.item())
+
+print(decode(m.generate(idx = torch.zeros((1, 1), dtype=torch.long), max_new_tokens=500)[0].tolist()))
+
+# toy example illustrating how matrix multiplication can be used for a "weighted aggregation"
+torch.manual_seed(42)
+a = torch.tril(torch.ones(3, 3))
+a = a / torch.sum(a, 1, keepdim=True)
+b = torch.randint(0,10,(3,2)).float()
+c = a @ b
+print('a=')
+print(a)
+print('--')
+print('b=')
+print(b)
+print('--')
+print('c=')
+print(c)
+
+
+torch.manual_seed(1337)
+B,T,C = 4,8,2 # batch, time, channels
+x = torch.randn(B,T,C)
+x.shape
+
+wei = torch.tril(torch.ones(T, T))
+wei = wei / wei.sum(1, keepdim=True)
+xbow2 = wei @ x # (B, T, T) @ (B, T, C) ----> (B, T, C)
+torch.allclose(xbow, xbow2)
+
+tril = torch.tril(torch.ones(T, T))
+wei = torch.zeros((T,T))
+wei = wei.masked_fill(tril == 0, float('-inf'))
+wei = F.softmax(wei, dim=-1)
+xbow3 = wei @ x
+torch.allclose(xbow, xbow3)
+
+torch.manual_seed(1337)
+B,T,C = 4,8,32 # batch, time, channels
+x = torch.randn(B,T,C)
+
+# let's see a single Head perform self-attention
+head_size = 16
+key = nn.Linear(C, head_size, bias=False)
+query = nn.Linear(C, head_size, bias=False)
+value = nn.Linear(C, head_size, bias=False)
+k = key(x)   # (B, T, 16)
+q = query(x) # (B, T, 16)
+wei =  q @ k.transpose(-2, -1) # (B, T, 16) @ (B, 16, T) ---> (B, T, T)
+
+tril = torch.tril(torch.ones(T, T))
+#wei = torch.zeros((T,T))
+wei = wei.masked_fill(tril == 0, float('-inf'))
+wei = F.softmax(wei, dim=-1)
+
+v = value(x)
+out = wei @ v
+#out = wei @ x
+
+out.shape
+
+wei[0]
