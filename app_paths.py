7e9f86aab651863a19477869fbe4f62b9b07b392
Raj Rawariya <98628762+Raj100@users.noreply.github.com>
2023-11-13 01:25:48 +0530

    commit #9

---
+++ app_paths.py
@@ -0,0 +1,3 @@
+apps = [["music", "/System/Applications/Music.app"],
+                ["powerpoint", "/Applications/\"Microsoft PowerPoint.app\""],
+                ["vs code", "/Applications/\"Visual Studio Code.app\""]]
